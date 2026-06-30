2cf4327027d472079a928a61f626133709cbc50e
Deccandental <dr.nanjapa@mydeccandental.com>
2026-06-30 00:46:54 -0700

    Replace HomeCalendar with PublicCalendar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Sidebar } from "@/components/Sidebar";
-import HomeCalendar from "@/components/HomeCalendar";
+import PublicCalendar from "@/components/PublicCalendar";
 
 export default function Home() {
   return (
     <main className="min-h-screen" style={{ background: "#f5f5f5" }}>
       <Sidebar />
       <div className="ml-64 p-8">
-        <HomeCalendar />
+        <PublicCalendar />
       </div>
     </main>
   );
