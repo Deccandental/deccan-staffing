5f883211049c167eb53c48a2ecb752d3dcbc6b7c
Deccandental <dr.nanjapa@mydeccandental.com>
2026-06-30 13:35:16 -0700

    Adjust layout padding and margin in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen" style={{ background: "#f5f5f5" }}>
       <Sidebar />
-      <div className="ml-64 p-8">
+      <div className="pt-16 lg:pt-0 lg:ml-64 p-4 lg:p-8">
         <PublicCalendar />
       </div>
     </main>
